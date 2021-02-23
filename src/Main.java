a2ffafc87e732ad7dd7487b490ce19bde7c6ba52
Raluca Vultur <filip.raluca97@gmail.com>
2021-02-23 10:52:25 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        Logger logger1 = Logger.getInstance();
+
+        System.out.println(logger.equals(logger1));
+    }
+}
